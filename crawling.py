b8c37169a6091c1f622953b46404253752d6aa04
jichanggyu <sa3590@naver.com>
2018-12-20 17:16:11 +0900

    Update crawling.py

--- crawling.py
+++ crawling.py
@@ -1,8 +1,11 @@
-
+# -*- coding: utf-8 -*-
 import urllib.request
 from bs4 import BeautifulSoup
+from requests import get
+import requests
 
-
+import sys
+sys.getdefaultencoding()
 
 def crawling(category):
 
@@ -70,13 +73,45 @@ def make_txt_crawling(url,category):
     fw.write(text)
     fw.close()
 
-crawling('category_society')
-crawling('category_politics')
-crawling('category_economic')
-crawling('category_foreign')
-crawling('category_culture')
-crawling('category_entertain')
-crawling('category_digital')
-crawling('category_sports')
-crawling('category_editorial')
-crawling('category_press')
+# crawling('category_society')
+# crawling('category_politics')
+# crawling('category_economic')
+# crawling('category_foreign')
+# crawling('category_culture')
+# crawling('category_entertain')
+# crawling('category_digital')
+# crawling('category_sports')
+# crawling('category_editorial')
+# crawling('category_press')
+
+
+def search_crawling(word):
+    print(word)
+    list_href = []
+    list_content = []
+    url = "http://search.chosun.com/search/news.search?query={}&pageno=".format(word)
+    url2 = "&orderby=news&naviarraystr=&kind=&cont1=&cont2=&cont5=&categoryname=%EC%A1%B0%EC%84%A0%EC%9D%BC%EB%B3%B4&categoryd2=&c_scope=navi&sdate=&edate=&premium="
+
+    # 페이지 href 찾는부분
+    for i in range(0, 1):
+        urls = url + str(i) + url2
+        r = get(urls)
+        print(urls)
+        soup = BeautifulSoup(r.content.decode('euc-kr','replace'))
+        for ul_tags in soup.find_all("div", class_='search_news_box'):
+            for dt_tags in ul_tags.find_all("dt"):
+                for a_tags in dt_tags.find_all("a"):
+                    list_href.append(a_tags["href"])
+    print(list_href)
+    # href에 있는 내용 부분 크롤링
+    for urlss in list_href:
+        soup = BeautifulSoup(urllib.request.urlopen(urlss).read(), "html.parser")
+        for content in soup.find_all("div", class_="par"):
+            list_content.append(content.get_text())
+
+    print(list_content)
+    # list -> 문자열로 바꿈
+    text = " ".join(list_content)
+
+    return text
+
