abf635c6d76179bb50c9cd9f3ee69303c2918472
Yongjae <coaleeyong@gmail.com>
2018-12-20 14:09:50 +0900

    slackbot answering cycle made

--- flask_server.py
+++ flask_server.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, request, make_response, jsonify
 from slacker import Slacker
+import requests, json
+from slackclient import SlackClient
 
 app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
 
 # 슬랙 토큰으로 객체 생성
 token = "xoxb-503818135714-509602121223-xBDZOPzJ76fRRVmnAHDaYquv"
+slack_verification = "869x6D9L8QjN08ciUCRnyodW"
 slack = Slacker(token)
+sc = SlackClient(token)
 
 @app.route('/')
 def index():
@@ -13,18 +18,91 @@ def index():
 
 @app.route('/listening', methods=["GET", "POST"])
 def listening():
-    pass
+    slack_event = json.loads(request.data)
+
+    if "challenge" in slack_event:
+        return make_response(slack_event["challenge"], 200, {"content_type":
+                                                                 "application/json"
+                                                             })
+
+    if slack_verification != slack_event.get("token"):
+        message = "Invalid Slack verification token: %s" % (slack_event["token"])
+        make_response(message, 403, {"X-Slack-No-Retry": 1})
+
+    if "event" in slack_event:
+        event_type = slack_event["event"]["type"]
+        return _event_handler(event_type, slack_event)
+
+    # If our bot hears things that are not events we've subscribed to,
+    # send a quirky but helpful error response
+    return make_response("[NO EVENT IN SLACK REQUEST] These are not the droids\
+                         you're looking for.", 404, {"X-Slack-No-Retry": 1})
 
 def _event_handler(event_type, slack_event):
-    pass
+    if event_type == "app_mention":
+        channel = slack_event["event"]["channel"]
+        text = slack_event["event"]["text"]
 
-def _get_intent(slack_msg, user_key):
-    pass
+        result_df = _get_answer_from_DF(text[13:], 'random_session')
 
+        speech = result_df['speech']
+        intent = result_df['intent']
+
+        if intent == 'category_society':
+            result_process = _call_process()
+
+        #slack.chat.post_message(channel, '{0}\n\n{1}'.format(speech, result_process))
+
+        sc.api_call(
+            "chat.postMessage",
+            channel=channel,
+            text='{0}\n\n{1}'.format(speech, result_process)
+        )
+
+        return make_response("App mention message has been sent", 200)
+
+    # ============= Event Type Not Found! ============= #
+    # If the event_type does not have a handler
+    message = "You have not added an event handler for the %s" % event_type
+    # Return a helpful error message
+    return make_response(message, 200, {"X-Slack-No-Retry": 1})
+
+def _get_answer_from_DF(slack_msg, user_key):
+    data_send = {
+        'query': slack_msg,
+        'sessionId': user_key,
+        'lang': 'ko',
+    }
+
+    data_header = {
+        'Authorization': 'Bearer 22c41598e8464addbc40892602a5ea3d',
+        'Content-Type': 'application/json; charset=utf-8'
+    }
+
+    dialogflow_url = 'https://api.dialogflow.com/v1/query?v=20150910'
+    res = requests.post(dialogflow_url, data=json.dumps(data_send), headers=data_header)
+
+    if res.status_code != requests.codes.ok:
+        return '오류가 발생했습니다.'
+
+    data_receive = res.json()
+    result = {
+        "speech": data_receive['result']['fulfillment']['speech'],
+        "intent": data_receive['result']['metadata']['intentName']
+    }
+
+    return result
+
+# Wrapper
+def _call_crawler():
+    return "crawler called"
+
+# Wrapper
+def _call_process():
+    return _call_crawler() + "->" + "process processed"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
-
 
 '''Slacker example
 # 메시지 전송 (#채널명, 내용)
